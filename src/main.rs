024a18b884388f775b43f6d7a2e27ae88451b8f6
Rumen <rumenmitov@protonmail.com>
2024-04-28 18:32:52 +0200

    version 3

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-use std::io;
-
-use xml_interpreter::*;
-
-
 fn main() {
-    let mut input = String::new();
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read from stdin.");
-
-    input = input
-        .trim()
-        .replace("\t", "");
-
-    let root = Element::new_root();
-
-    if let Ok((root, _)) = parse(&input, root) {
-        print!("{}", root.print(0));
-    }
+    println!("Hello, world!");
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,89 +1,9 @@
 #[cfg(test)]
 use super::*;
 
-
 #[test]
-fn test_parse() {
-    let mut element = Element::new_root();
+fn test_element_tree() {
 
-    let mut element_solution = Element::new_root();
-    element_solution.children = vec!(
-        Element {
-            name: "hello".to_string(),
-            attributes: Vec::new(),
-            children: Vec::new()
-        }
-        );
-
-    assert_eq!(
-        parse("<hello></hello>", element).unwrap(),
-        (element_solution, "".to_string())
-        );
-
-    element = Element::new_root();
-
-    element_solution = Element::new_root();
-    element_solution.children = vec!(
-        Element {
-            name: "hello".to_string(),
-            attributes: vec!(
-                ("size".to_string(), "10".to_string()),
-                ("text".to_string(), "5".to_string())
-                ),
-                children: vec!(
-                    Element {
-                        name: "nested".to_string(),
-                        attributes: vec!(),
-                        children: vec!(),
-                    }
-                    ),
-        }
-        );
-
-    assert_eq!(
-        parse("<hello size=10 text=5><nested></nested></hello>", element).unwrap(),
-        (element_solution, "".to_string())
-        );
+    let et = ElementTree::new("<root </root").unwrap();
+    assert_eq!("root", et.elements_table[0].name);
 }
-
-
-#[test]
-fn test_extract_attribute() {
-    assert_eq!(
-        extract_attributes("hello=world text=5>"),
-        Ok(Box::from((
-                    vec!(
-                        ("hello".to_string(), "world".to_string()),
-                        ("text".to_string(), "5".to_string())
-                        ), 
-                    String::from("")
-                    )))
-        );
-
-    assert_eq!(
-        extract_attributes("!hello"),
-        Err("Error parsing attribute: !hello".to_string())
-        );
-}
-
-
-#[test]
-fn test_extract_name() {
-    assert_eq!(
-        extract_name("hello>"),
-        Ok(
-            Box::from(("hello".to_string(), ">".to_string())))
-        );
-
-    assert_eq!(
-        extract_name("hello/>"),
-        Ok(
-            Box::from(("hello".to_string(), "/>".to_string())))
-        );
-
-    assert_eq!(
-        extract_name("5hello>"),
-        Err("Invalid character when extracting name: 5".to_string())
-        );
-}
-
